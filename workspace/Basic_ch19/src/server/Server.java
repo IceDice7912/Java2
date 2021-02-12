660fb50a97009052653daa3c9f56c2154d1e2fa1
IceDice <dice7912@naver.com>
2021-02-13 04:03:00 +0900

    SC modiefy

--- workspace/Basic_ch19/src/server/Server.java
+++ workspace/Basic_ch19/src/server/Server.java
@@ -11,13 +11,15 @@ public class Server {
 		// TODO Auto-generated method stub
 		try {
 			ServerSocket ss = new ServerSocket(1234);
-			Socket s = ss.accept();
-			System.out.println(s.getPort());
-			System.out.println(s.getInetAddress()+"connect ok");
 			
+			while(true) {
+				Socket s = ss.accept();
+				System.out.println(s.getPort());
+				System.out.println(s.getInetAddress()+"connect ok");
 			DataInputStream in = new DataInputStream(s.getInputStream());
 			String msg = in.readUTF();
 			System.out.println(msg);
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
